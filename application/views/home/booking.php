7d48666d6e9c1ec1a7f919567f0907a87382c184
Karrottu <dru.tanti@gmail.com>
2019-05-24 20:26:03 +0200

    Almost Done

--- application/views/home/booking.php
+++ application/views/home/booking.php
@@ -1,38 +1,30 @@
 <div class="container-fluid" id="content">
     <div class="row">
-        <main class="col-12 col-md-8 my-4">
-            <div class="page-header">
-                <h1>Main Content</h1>
+        <main class="col-10 my-4">
+            <div class="row d-flex justify-content-center">
+                    <img src="<?php echo base_url().'/images/screen.png' ?>" alt="Screen" style="width: 50%; height: 50%">
+               <table>
+<?php
+   $cols = $cinema['capacity'] / $cinema['num_rows'];
+   for ($row = 0; $row < $cinema['num_rows']; $row++):
+?>
+                   <tr>
+<?php
+   for ($col = 0; $col < $cols; $col++):
+       $seat = ($row * $cols) + $col;
+?>
+                       <td>
+                           <input type="checkbox" name="booking-seat[]" class="mx-3 my-2" value="<?php echo $seat; ?>">
+                       </td>
+<?php endfor; ?>
+                   </tr>
+<?php endfor; ?>
+               </table>
+
             </div>
-            <div class="row">
-               <div class="col-md-6">
-                   <table align="center">
-   <?php
-       $cols = $cinema['capacity'] / $cinema['num_rows'];
-       for ($row = 0; $row < $cinema['num_rows']; $row++):
-   ?>
-                       <tr>
-   <?php
-       for ($col = 0; $col < $cols; $col++):
-           $seat = ($row * $cols) + $col;
-   ?>
-                           <td>
-                               <input type="checkbox" name="booking-seat[]" class="mx-3 my-2" value="<?php echo $seat; ?>">
-                           </td>
-   <?php endfor; ?>
-                       </tr>
-   <?php endfor; ?>
-                   </table>
-
-               </div>
-
+            <div class="mt-5">
+                <button type="submit" class="btn btn-lg btn-secondary w-100">Submit</button>
             </div>
         </main>
-        <aside class="sidebar col-12 col-md-2 d-block m-4">
-            <div class="page-header">
-                <h1>Sidebar</h1>
-            </div>
-
-        </aside>
     </div>
 </div>
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,21 +1,22 @@
 
-        <nav class = "navbar navbar-expand-lg sticky-top border-bottom py-1">
-            <a href="<?php echo site_url('home') ?>" class="navbar-brand ml-4">
-                <img src="<?php echo base_url('images/logo.png') ?>" width = "64" height= "64" alt="Eden Cinemas Logo" class="d-sm-block">
-            </a>
-            <div class="nav-container d-flex mt-2 mt-lg-0 ml-3">
-                <a class="nav-link px-5" href="#">Now Showing</a>
-                <a class="nav-link px-5" href="#">Coming Soon</a>
-                <a class="nav-link px-5" href="#">Contact Us</a>
-                <a class="nav-link px-5" href="<?php echo site_url('booking') ?>">Disabled</a>
-            </div>
-            <div class="nav-container">
-                <?php $is_logged = $this->system->confirm_session();
-                if(!$is_logged):
-                    ?>
-                    <a class="nav-link px-4" href="<?php echo site_url('login') ?>">Login</a>
-                <?php else: ?>
-                    <a href="<?php echo site_url('logout') ?>" class="nav-link px-4"><i class="fas fa-user"></i> Logout</a>
-                <?php endif; ?>
+        <nav class="header">
+            <div class="row nav-container mx-4">
+                <div class="col d-flex flex-column justify-content-center">
+                    <a href="<?php echo site_url('home') ?>" class="navbar-brand">
+                        <img src="<?php echo base_url('images/logo.png') ?>" width = "72" height= "72" alt="Eden Cinemas Logo" class="">
+                    </a>
+                </div>
+                <div class="col d-flex flex-column justify-content-center">
+                    <a href="#"><h5>Now Showing</h5></a>
+                </div>
+                <div class="col d-flex flex-column justify-content-center">
+                    <?php $is_logged = $this->system->confirm_session();
+                    if(!$is_logged):
+                        ?>
+                        <a href="<?php echo site_url('login') ?>"><h6>Login</h6></a>
+                    <?php else: ?>
+                        <a href="<?php echo site_url('logout') ?>"><i class="fas fa-user"></i> <h6>Logout</h6></a>
+                    <?php endif; ?>
+                </div>
             </div>
         </nav>
